abfa2d46fbdbfa3ac046122cc4b4112aa5437a23
Julio Cesar <julioosilva97@gmail.com>
2021-03-14 23:03:20 -0300

    Login, JWT token

---
+++ hr-oauth/src/main/java/com/julioosilva97/hroauth/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.julioosilva97.hroauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("MY-SECRET-KEY");
+		return accessTokenConverter;
+	}
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+}
--- hr-oauth/src/main/java/com/julioosilva97/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/julioosilva97/hroauth/services/UserService.java
@@ -1,13 +1,17 @@
 package com.julioosilva97.hroauth.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.ctc.wstx.util.SymbolTable;
 import com.julioosilva97.hroauth.entities.User;
 import com.julioosilva97.hroauth.faignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	@Autowired
 	private UserFeignClient userFeignClient;
@@ -16,6 +20,15 @@ public class UserService {
 		User user = userFeignClient.findByEmail(email).getBody();
 		
 		if(user == null)  throw new IllegalArgumentException("Email not found");
+		
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+         User user = userFeignClient.findByEmail(username).getBody();
+		System.out.println(user);
+		if(user == null)  throw new UsernameNotFoundException("Email not found");
 		
 		return user;
 	}
